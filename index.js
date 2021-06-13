1ac615faa9aab416d5c4d738ac2f81b2c8041970
nidhimakdani <50043246+nidhimakdani@users.noreply.github.com>
2021-06-13 13:27:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+//author @nidhimakdani
+
+//undo comment of a section yopu want to run (recomended : Consol(browser))
+
+//alert("Hello");
+//alert("World");
+
+//a=10;
+//typeof(a);
+
+//prompt("What is your name?");
+
+//var name="nidhi";
+//name
+//alert(name)
+
+//var yname = prompt("what is your name");
+//alert("Welcome "+yname);
+
+// var a = "3";
+// var b = "8";
+// var c = a;
+// a = b;
+// b = c;
+// console.log("a is " + a);
+// console.log("b is " + b);
+
+
+
