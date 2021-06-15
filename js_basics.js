73952518ccde714ecfddea65cf86152c4e745f58
nidhimakdani <50043246+nidhimakdani@users.noreply.github.com>
2021-06-15 23:53:14 +0530

    Update and rename index.js to js_basics.js

---
+++ js_basics.js
@@ -0,0 +1,164 @@
+//author @nidhimakdani
+
+//undo comment of a section yopu want to run (recomended : Consol(browser))
+
+//.....................
+
+// console.log("hi");
+// document.write("Writing");
+// console.warn("This is Warning");
+// console.error("This is Error");
+
+//alert("Hello");
+//alert("World");
+
+//variables
+// var a=9;
+// var b=10;
+// console.log(a+b);
+
+//Data types
+// var s1 ="This is a string";
+// var n1 =12;
+// //Objects
+// var marks ={
+//     nidhi:96,
+//     kruti:97
+// }
+// console.log(marks);
+// //Boolean
+// var a = true;
+// console.log(a)
+// //undefined
+// var b;
+// console.log(b)
+//2 types of data types
+/*
+1.primitive undefined,null,number,string,boolean
+2.reference objects and arrays
+*/
+
+//a=10;
+//typeof(a);
+
+//prompt("What is your name?");
+//var name="nidhi";
+//name
+//alert(name)
+
+//var yname = prompt("what is your name");
+//alert("Welcome "+yname);
+
+// var a = "3";
+// var b = "8";
+// var c = a;
+// a = b;
+// b = c;
+// console.log("a is " + a);
+// console.log("b is " + b);
+
+// alert("hello " + " world!");
+
+// var msg="Good Afternoon";
+// var name="Nidhi"
+// alert(msg + " " + name);
+
+// name = "Nidhi";
+// name.length;
+
+// Small exersise to find how many character are written and how many characters are left!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// var tweet = prompt("write your tweet here");
+//  tweet.slice(0,120)
+// alert("you have writter " + tweet.length + " characters and " + (120-tweet.length) + " character is left!");
+
+// var name = "NIdhi"
+// name.slice(0,4)
+
+// var name = prompt("Enter Your Name");
+// var p1 = name.slice(0,1).toUpperCase();
+// var p2 = name.slice(1,name.length).toLowerCase();
+// alert("Hey " + p1+p2 + " Good Evening!");
+
+//String Methods
+// 1.indexOf
+// 2.lastIndexOf
+// 3.slice
+// 4.replace
+
+//Loops
+//For Loop
+var arr=[1,2,3,4,5];
+/*
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
+*/
+//For each
+/*
+arr.forEach(function(element) {
+    console.log(element)
+});
+// */
+// let j=0; //block level scope where is old
+// const a=0;
+// a = a+1; 
+//while
+// let j=0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+// // Do while
+// do{
+//     console.log(arr[i]);
+//     j++;
+// }while(j<arr.length);
+//push pop shift unshift
+
+//Arrays
+// var arr=[1,2,3,4]
+// console.log(arr)
+
+//.....................
+
+//Operators
+/*
+1.Arithmetic
+2.Assignment
+3.Comparison
+4.Logical Operators (or and logical or(true false false true))
+*/
+
+
+//Function
+// function avg(a,b){
+//     return(a+b)/2;
+// }
+// c1= avg(50,18);
+// c1= avg(88,45);
+// console.log(c1,c2);
+//DRY : DO NOT REPEAT YOURSELF
+
+//.....................
+
+// var age = 12;
+// if(age>18){
+//     console.log("You are adult!");
+// }
+// else{
+//     console.log("You are kid!");
+// }
+
+//java script Dates
+// myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.getDay());
+// console.log(myDate.getFullYear());
+// console.log(myDate.GetMinutes());
+
+
+//-----------------------------------------------
+//Basics are Completed 
+//Happy Learning
+//Now Go For DOM and EVENTS
+
